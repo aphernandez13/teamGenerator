a474c2dd1314e8f0c8fe83a6b3de0ebb44b482cb
alfredo pedroza <alfredopedrozahernandez13@gmail.com>
2020-10-19 20:55:54 -0700

    fixed

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const path = require("path");
 const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+const team = []
 
 const {
   addEmployee,
@@ -9,7 +18,10 @@ const {
 } = require("./prompt");
 
 managerPrompt().then(function (manager) {
-  //build a manager object, and push into employees array
+  team.push(
+    new Manager(manager.name, manager.id, manager.email, manager.officeNumber)
+  );
+
   console.log(manager);
   main();
 });
@@ -19,21 +31,25 @@ function main() {
     if (engineerOrIntern === "Engineer") {
       //prompt for engineer details
       engineerPrompt().then(function (engineer) {
-        //build engineer add to employees array
+        team.push(
+          new Engineer(engineer.name, engineer.id, engineer.email, engineer.github)
+        );
         console.log(engineer);
         main();
       });
     } else if (engineerOrIntern === "Intern") {
       //prompt for intern info
       internPrompt().then(function (intern) {
-        //build engineer add to employees array
+        team.push(
+          new Intern(intern.name, intern.id, intern.email, intern.school)
+        );
         console.log(intern);
         main();
       });
     } else {
       //write to file
 
-      const team = render(employees);
+      const team = render(team);
       fs.writeFile("main.html", team, function (err) {
         if (err) {
           throw err;
